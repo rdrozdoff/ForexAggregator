a871049ea4ba8c5658652f61957e6e17fa17d1ae
rdrozdoff <rdrozdoff@gmail.com>
2020-05-07 11:49:40 +0300

    minor

--- services/data-provider/src/main/java/org/home/forex/controller/DataController.java
+++ services/data-provider/src/main/java/org/home/forex/controller/DataController.java
@@ -3,9 +3,10 @@ package org.home.forex.controller;
 import org.apache.commons.math3.util.Precision;
 import org.home.forex.model.Quote;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 import java.util.Random;
@@ -25,13 +26,10 @@ public class DataController {
         double close = generatePrice(basePrice);
         double high = generatePrice(basePrice);
         double low = generatePrice(basePrice);
-
         high = DoubleStream.of(open, close, high, low).max().getAsDouble();
         low = DoubleStream.of(open, close, high, low).min().getAsDouble();
 
-        return new ResponseEntity<>(
-                new Quote(symbol, Instant.now().toEpochMilli(), open, close, high, low),
-                HttpStatus.OK);
+        return ResponseEntity.ok(new Quote(symbol, Instant.now().toEpochMilli(), open, close, high, low));
     }
 
     private double generatePrice(double basePrice) {
